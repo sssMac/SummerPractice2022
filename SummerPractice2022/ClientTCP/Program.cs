838dbb8edf22a2cd2c62b7b76d276c6d6821ff41
Ilya <ilya-lyapin@mail.ru>
2022-07-01 05:37:11 +0300

    Feature: init tcp client server

---
+++ SummerPractice2022/ClientTCP/Program.cs
@@ -0,0 +1,41 @@
+﻿using SuperSimpleTcp;
+using System.Text;
+
+try
+{
+    SimpleTcpClient client = new SimpleTcpClient("127.0.0.1:8000");
+
+    string command = "";
+
+    client.Events.Connected += Connected;
+    client.Events.Disconnected += Disconnected;
+    client.Events.DataReceived += DataReceived;
+
+    client.Connect();
+
+    while (!command.Equals("exit"))
+    {
+        command = Console.ReadLine();
+        client.Send(command);
+    }
+
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+
+static void Connected(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine($"*** Server {e.IpPort} connected");
+}
+
+static void Disconnected(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine($"*** Server {e.IpPort} disconnected");
+}
+
+static void DataReceived(object sender, DataReceivedEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}] {Encoding.UTF8.GetString(e.Data)}");
+}
---
+++ SummerPractice2022/ServerTCP/Program.cs
@@ -0,0 +1,44 @@
+﻿using SuperSimpleTcp;
+using System.Net;
+using System.Text;
+
+try
+{
+    SimpleTcpServer server = new SimpleTcpServer("*", 8000);
+
+    string command = "";
+
+    server.Events.ClientConnected += ClientConnected;
+    server.Events.ClientDisconnected += ClientDisconnected;
+    server.Events.DataReceived += DataReceived;
+
+    server.Start();
+
+    while (!command.Equals("exit"))
+    {
+        command = Console.ReadLine();
+        server.Send("[ClientIp:Port]", command);
+
+    }
+
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+static void ClientConnected(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}] client connected");
+}
+
+static void ClientDisconnected(object sender, ConnectionEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}] client disconnected: {e.Reason}");
+}
+
+static void DataReceived(object sender, DataReceivedEventArgs e)
+{
+    Console.WriteLine($"[{e.IpPort}]: {Encoding.UTF8.GetString(e.Data)}");
+}

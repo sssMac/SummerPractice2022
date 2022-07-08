3e18f5930d99f07249a7be95970611a5de2a4588
sssMac <sssMac@github.com>
2022-07-08 14:33:21 +0300

    Feature(json) : json update

---
+++ SummerPractice2022/JSONData/BoardCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONData
+{
+    public abstract class BoardCommand
+    {
+        public byte Code { get; protected set; }
+        public BoardCommand()
+        {
+            Code = 0x00;
+        }
+    }
+}
---
+++ SummerPractice2022/JSONData/EngineData/EngineControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONData.EngineData
+{
+    public class EngineControl
+    {
+        public byte EngineNumber { get; set; }
+        public byte Direct { get; set; }
+        public byte Speed { get; set; }
+    }
+}
